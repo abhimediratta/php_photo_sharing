a176c5c2f197997c4460a18ac467b511473b7ce9
Abhi Mediratta <abhimediratta@gmail.com>
2014-05-12 22:06:26 +0530

    deploy code

--- application/config/database.php
+++ application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = FALSE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'abhi';
-$db['default']['password'] = '';
-$db['default']['database'] = 'test';
+$db['default']['hostname'] = '127.11.122.2';
+$db['default']['username'] = 'adminSwtmUsl';
+$db['default']['password'] = 'ByYj_rwavWFX';
+$db['default']['database'] = 'php';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
